bcae237445e621f039bb9059198990934469c408
hanterold72-ui <hanterold72@gmail.com>
2026-09-10 13:57:57 +0300

    Update models.py

--- backend/app/models.py
+++ backend/app/models.py
@@ -17,8 +17,6 @@ engine = create_engine(
     max_overflow=3,
     connect_args={"check_same_thread": False} if "sqlite" in DATABASE_URL else {}
 )
-    connect_args={"check_same_thread": False} if "sqlite" in DATABASE_URL else {}
-)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
